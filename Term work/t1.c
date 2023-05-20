322087a8b3d912ff0dab7dd652ebaf1a989db60f
Raghvan-Dixit <123835048+Raghvan-Dixit@users.noreply.github.com>
2023-05-20 09:02:24 +0530

    question added

--- Term work/t1.c
+++ Term work/t1.c
@@ -1,3 +1,17 @@
+/*
+Q1. Write a C program to create a structure containing book information like 
+accession number, name of author, book title and flag to know whether book is 
+issued or not.
+Create a menu in which the following can be done.
+1 - Display book information
+2 - Add a new book
+3 - Display all the books in the library of a particular author
+4 - Display the number of books of a particular title
+5 - Display the total number of books in the library
+6 - Issue a book
+(If we issue a book, then its number gets decreased by 1 and if we add a book, its 
+number gets increased by 1)
+*/
 #include <stdio.h>
 #include <string.h>
 struct Book 
